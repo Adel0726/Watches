d4fb9c222e6786b52fb0745f724a11e9da5c85de
bardien <greger.grym@gmail.com>
2025-03-10 09:28:22 +0100

    siuuuu

--- index.js
+++ index.js
@@ -16,3 +16,50 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   
   });
+
+
+  
+
+
+
+  const productList = document.getElementById("product-list");
+  const productContainer = document.getElementById("product-container");
+  const leftBtn = document.getElementById("left-btn");
+  const rightBtn = document.getElementById("right-btn");
+
+  for (let i = 1; i <= 7; i++) {
+      let product = document.createElement("div");
+      product.classList.add("product");
+
+      product.innerHTML = `
+          <figure>
+              <img src="assets/watch${i}.jpg" alt="Watch ${i}">
+              <figcaption>Watch ${i}</figcaption>
+          </figure>
+      `;
+      productList.append(product);
+  }
+
+
+  function updateButtons() {
+     if (productContainer.scrollLeft <= 0) {
+          leftBtn.classList.add("invisible");
+      } else {
+          leftBtn.classList.remove("invisible");
+      } if (productContainer.scrollLeft + productContainer.clientWidth >= productContainer.scrollWidth) {
+          rightBtn.classList.add("invisible");
+      } else {
+      rightBtn.classList.remove("invisible");
+     }
+  }           
+
+
+  function scrollProducts(direction) {
+      productContainer.scrollBy({ left: direction * 250 , behavior: "smooth" });
+
+      updateButtons()
+  }
+
+  leftBtn.addEventListener("click", () => scrollProducts(-1));
+  rightBtn.addEventListener("click", () => scrollProducts(1));
+
